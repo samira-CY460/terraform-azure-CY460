dc17a1476c50f78dc4e643ca25f8a9ff474bb596
samiraCY460 <samira1401@hotmail.com>
2025-10-09 19:53:39 -0400

    Ajout Resource Group pour CR460

---
+++ resources.tf
@@ -0,0 +1,37 @@
+# Création d'un Storage Account dans le Resource Group existant
+resource "azurerm_storage_account" "sa_cr460" {
+  name                     = "stcr460samira"
+  resource_group_name      = azurerm_resource_group.rg_pipeline.name
+  location                 = azurerm_resource_group.rg_pipeline.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Environment = "Dev"
+    Project     = "CR460"
+    Owner       = "Samira"
+  }
+}
+
+# Sortie pour Terraform Cloud
+output "storage_account_name" {
+  value = azurerm_storage_account.sa_cr460.name
+}
+
+resource "azurerm_storage_account" "sa_cr460" {
+  name                     = "stcr460samira"
+  resource_group_name      = azurerm_resource_group.rg_pipeline.name
+  location                 = azurerm_resource_group.rg_pipeline.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Environment = "Dev"
+    Project     = "CR460"
+    Owner       = "Samira"
+  }
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa_cr460.name
+}
