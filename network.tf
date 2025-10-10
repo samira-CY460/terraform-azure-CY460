d19f4fdcbf165281d40e6b804c2a96f753fecfeb
samiraCY460 <samira1401@hotmail.com>
2025-10-09 21:13:04 -0400

    Ajout VMLinux avec réseau et clé SSH incluse

--- network.tf
+++ network.tf
@@ -1,13 +1,43 @@
+# Création du Subnet dans le réseau virtuel existant
+resource "azurerm_subnet" "subnet_cr460" {
+  name                 = "subnet-cr460"
+  resource_group_name  = azurerm_resource_group.rg_pipeline.name
+  virtual_network_name = azurerm_virtual_network.vnet_cr460.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Création d'une IP publique pour la VM
+resource "azurerm_public_ip" "pip_cr460" {
+  name                = "pip-cr460-samira"
+  resource_group_name = azurerm_resource_group.rg_pipeline.name
+  location            = azurerm_resource_group.rg_pipeline.location
+  allocation_method   = "Dynamic"
+}
+
+# Création de la Network Interface (NIC) pour la VM
+resource "azurerm_network_interface" "nic_cr460" {
+  name                = "nic-cr460-samira"
+  location            = azurerm_resource_group.rg_pipeline.location
+  resource_group_name = azurerm_resource_group.rg_pipeline.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet_cr460.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip_cr460.id
+  }
+}
+
 # Création d'une VM Linux
 resource "azurerm_linux_virtual_machine" "vm_cr460" {
   name                = "vm-cr460-samira"
   resource_group_name = azurerm_resource_group.rg_pipeline.name
   location            = azurerm_resource_group.rg_pipeline.location
-  size                = "Standard_B1s"   # petite VM pour test
+  size                = "Standard_B1s"
   admin_username      = "azureuser"
 
   network_interface_ids = [
-    azurerm_network_interface.nic_cr460.id,
+    azurerm_network_interface.nic_cr460.id
   ]
 
   admin_ssh_key {
@@ -33,4 +63,9 @@ resource "azurerm_linux_virtual_machine" "vm_cr460" {
     Project     = "CR460"
     Owner       = "Samira"
   }
+}
+
+# Sortie pour Terraform Cloud
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.vm_cr460.name
 }
