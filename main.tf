362212327ae92969ba91845a448e612603ea0e80
samiraCY460 <samira1401@hotmail.com>
2025-10-22 17:55:11 -0400

    ajout du déploiement d'un conteneur docker avec ACI M.A.J

--- main.tf
+++ main.tf
@@ -7,7 +7,21 @@
 # Création du Resource Group
 resource "azurerm_resource_group" "rg_pipeline" {
   name     = "rg-cr460-pipeline-samira"
-  location = "eastus"  # Tu peux changer la région si besoin (ex: canadacentral)
+  location = "Centralus"
+
+  tags = {
+    Environment = "Dev"
+    Project     = "CR460"
+    Owner       = "Samira"
+  }
+}
+
+# Création du Réseau Virtuel (corrigé avec le nom réel)
+resource "azurerm_virtual_network" "vnet_cr460" {
+  name                = "vnet-cr460-samira"  # Aligné avec Azure
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg_pipeline.location
+  resource_group_name = azurerm_resource_group.rg_pipeline.name
 
   tags = {
     Environment = "Dev"
@@ -19,4 +33,9 @@ resource "azurerm_resource_group" "rg_pipeline" {
 # Sortie affichée dans Terraform Cloud
 output "resource_group_name" {
   value = azurerm_resource_group.rg_pipeline.name
+}
+
+# Configuration du fournisseur
+provider "azurerm" {
+  features {}
 }
